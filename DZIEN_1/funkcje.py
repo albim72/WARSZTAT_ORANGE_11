04351bbc570b52657834d9bfb9711724fbd49ee7
albim72 <marcin.albiniak72@gmail.com>
2022-11-02 13:49:11 +0100

    Update funkcje.py

--- DZIEN_1/funkcje.py
+++ DZIEN_1/funkcje.py
@@ -1,3 +1,5 @@
+import math
+
 #funkcja 1
 n=4
 m = 9
@@ -37,3 +39,65 @@ print(gx(3))
 #wywołaj funkcję z wartościami n=9, m zostaw domyślną, k=11, b=2
 
 print(gx(9,k=11,b=2))
+print(gx(9,b=2,k=11))
+#print(gx(b=2,9,k=11))
+
+#funkcje anonimowe lambda:
+
+print((lambda e:e**3)(5))
+
+def ex(e):
+    return e**3
+
+print(ex(5))
+
+s = lambda e,f=2:e-f
+print(s(3,7))
+print(s(3))
+
+
+def ob(k):
+    return lambda a,b:a**k-b
+
+print(ob(5)(1,2))
+
+def zw(k):
+    return k
+
+print((lambda a,b,k:a**zw(k)-b)(1,2,5))
+
+num = [67,2,5,-177,34,122,4,7,1,100,-4,5,0]
+#stwórz listę nparz i przekaż do niej wszystkie wartości parzyste z listy num
+#użyj funkcji standardowej filter (funkcja wyższego rzędu -> gdzie parametr 0 funkcji to inna funkcja,
+#paramet 1 to żródło danych)
+
+nparz = list(filter(lambda x:x%2==0,num))
+print(nparz)
+
+#stwórz listę cube i przekaż do niej wszystkie wartości z listy num podniesione do potęgi 3
+
+cube = list(map(lambda x:x**3,num))
+print(cube)
+
+def dodaj(x):
+    return x + 33
+
+trojka = list(map(dodaj,num))
+print(trojka)
+
+#własna funkcja wyższego rzędu
+
+def filtrowanie(dane,test):
+
+    plus = []
+    for element in dane:
+        if (test(element)):
+            plus.append(element)
+    return plus
+
+def ekstra_liczba(n):
+    return not n>=100
+
+print(filtrowanie(num,ekstra_liczba))
+print(filtrowanie(num,lambda x:x>=88))
+
