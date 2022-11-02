bb83afb0562008a38a9841aa276b591f2bf70767
albim72 <marcin.albiniak72@gmail.com>
2022-11-02 13:17:49 +0100

    Update funkcje.py

--- DZIEN_1/funkcje.py
+++ DZIEN_1/funkcje.py
@@ -24,3 +24,16 @@ def zewnetrzna():
 
 zewnetrzna()
 
+#funkcja 3
+
+def gx(n,m=1,k=3,b=5):
+    return math.sqrt(n+m)*k-b
+
+print(gx(3,6,8,1))
+print(gx(3,6,8))
+print(gx(3,6))
+print(gx(3))
+
+#wywołaj funkcję z wartościami n=9, m zostaw domyślną, k=11, b=2
+
+print(gx(9,k=11,b=2))
