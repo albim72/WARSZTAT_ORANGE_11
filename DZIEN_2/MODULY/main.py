f1d0fc358a2b2ce1f3dc82d774fbdfb29a1d7e6d
albim72 <marcin.albiniak72@gmail.com>
2022-11-03 11:40:40 +0100

    Update main.py

--- DZIEN_2/MODULY/main.py
+++ DZIEN_2/MODULY/main.py
@@ -4,6 +4,7 @@ import sys
 #import dane as dn
 from dane import nb,book
 from mfunkcje_lib.bfunkcje import czytaj, czytaj_dict
+from cls.sdane.czytaniedanych import CDane
 
 print(nb)
 print(book)
@@ -11,3 +12,9 @@ print(sys.path)
 
 czytaj(nb)
 czytaj_dict(book)
+
+cd = CDane(nb,book)
+print("_________________wypisanie za pomocą obiektu klasy CDane_______________________")
+
+cd.czytaj_lista()
+cd.czytaj_slow()
