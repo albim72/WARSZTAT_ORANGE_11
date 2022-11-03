65bb807cb6dbfa5a0e6a4be7e04b59017cd6768b
albim72 <marcin.albiniak72@gmail.com>
2022-11-03 15:47:11 +0100

    Update resistor.py

--- DZIEN_2/RESISTOR_PROJEKT/resistor.py
+++ DZIEN_2/RESISTOR_PROJEKT/resistor.py
@@ -1,6 +1,9 @@
 class Resistor:
-    
+
     def __init__(self,ohms):
         self.ohms = ohms
         self.voltage = 0
         self.current = 0
+    def __repr__(self):
+        return f'Resistor -> {self.__class__}(ohms={self.ohms}, voltage: {self.voltage})'
+
