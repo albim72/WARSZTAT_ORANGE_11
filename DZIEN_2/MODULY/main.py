1a8f7f4c92daa4d86953de95dca1d23fba59d35a
albim72 <marcin.albiniak72@gmail.com>
2022-11-03 11:29:40 +0100

    Create main.py

---
+++ DZIEN_2/MODULY/main.py
@@ -0,0 +1,13 @@
+import numpy
+import sys
+#import dane
+#import dane as dn
+from dane import nb,book
+from mfunkcje_lib.bfunkcje import czytaj, czytaj_dict
+
+print(nb)
+print(book)
+print(sys.path)
+
+czytaj(nb)
+czytaj_dict(book)
