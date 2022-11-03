ef60e67905d7ff8b6c5b880b66b0c9a684fa5e2d
albim72 <marcin.albiniak72@gmail.com>
2022-11-03 16:44:53 +0100

    Update rod.py

--- DZIEN_2/GraOTron/rod.py
+++ DZIEN_2/GraOTron/rod.py
@@ -1,7 +1,7 @@
 from abc import ABC,abstractmethod
 
 class CzlonekRodu(ABC):
-    
+
     def __init__(self,nazwa_rodu,tytul,plec,punkty_walki,punkty_palacowe,doswiadczenie):
         self.doswiadczenie = doswiadczenie
         self.punkty_palacowe = punkty_palacowe
@@ -9,15 +9,21 @@ class CzlonekRodu(ABC):
         self.plec = plec
         self.tytul = tytul
         self.nazwa_rodu = nazwa_rodu
-        
+
+    def __repr__(self):
+        return f"Ród: {self.nazwa_rodu}, tytuł -> {self.tytul} {self.__class__.__name__}"
+
     @abstractmethod
     def walka(self):
         pass
-    
+
     @abstractmethod
     def negocjacja(self):
         pass
-        
+
     @abstractmethod
     def uczta(self):
         pass
+
+
+
