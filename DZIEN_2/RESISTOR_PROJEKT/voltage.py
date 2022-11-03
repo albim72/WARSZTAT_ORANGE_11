c91219a18cf979cf39777e8de11e4bd2fc7dfd59
albim72 <marcin.albiniak72@gmail.com>
2022-11-03 15:33:27 +0100

    Update voltage.py

--- DZIEN_2/RESISTOR_PROJEKT/voltage.py
+++ DZIEN_2/RESISTOR_PROJEKT/voltage.py
@@ -1,15 +1,18 @@
 from resistor import Resistor
 
 class VoltageResistor(Resistor):
-    
+
     def __init__(self,ohms):
         super().__init__(ohms)
-        self._voltage = 0
-        
+        self._voltage = 5
+
+    def __repr__(self):
+        return f'VoltageResistor(ohms={self.ohms}, voltage: {self.voltage})'
+
     @property
     def voltage(self):
         return self._voltage
-    
+
     @voltage.setter
     def voltage(self,voltage):
         self._voltage = voltage
