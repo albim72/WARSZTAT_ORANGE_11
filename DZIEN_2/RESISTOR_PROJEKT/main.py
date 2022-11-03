0e51f2d942d2e3e2a2b871c1cad36dbe5e4970ce
albim72 <marcin.albiniak72@gmail.com>
2022-11-03 14:28:55 +0100

    Update main.py

--- DZIEN_2/RESISTOR_PROJEKT/main.py
+++ DZIEN_2/RESISTOR_PROJEKT/main.py
@@ -1,1 +1,15 @@
 
+from oldresistor import OldResistor
+
+print("_________________________________")
+
+r0 = OldResistor(10.3E2)
+print(r0)
+print(r0._ohms)
+r0._ohms = 2.66E2
+print(r0._ohms)
+r0.set_ohms(4.5E3)
+print(r0._ohms)
+print(r0.get_ohms())
+
+print("_________________________________")
