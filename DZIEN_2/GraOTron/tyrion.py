2bf1aa0ea4e1987bbfc792dc1474a2d3680c9c7e
albim72 <marcin.albiniak72@gmail.com>
2022-11-03 16:38:00 +0100

    Update tyrion.py

--- DZIEN_2/GraOTron/tyrion.py
+++ DZIEN_2/GraOTron/tyrion.py
@@ -4,18 +4,18 @@ class Tyrion(CzlonekRodu):
 
     def walka(self):
         if self.punkty_walki > self.punkty_palacowe and self.punkty_walki>=6:
-            print("Lord wysłany do walki...")
+            print(f"{self.tytul} wysłany do walki...")
         else:
-            print("Lord pozostaje w pałacu....")
+            print(f"{self.tytul} pozostaje w pałacu....")
 
     def negocjacja(self):
         if self.punkty_palacowe >= 8:
-            print("Lord wysłany do negocjacji.....")
+            print(f"{self.tytul} wysłany do negocjacji.....")
         else:
-            print("Lord wysłany do walki")
+            print(f"{self.tytul} wysłany do walki")
 
     def uczta(self):
         if self.punkty_palacowe >= 6 and self.doswiadczenie >= 7:
-            print("Lord jedzie na ucztę....")
+            print(f"{self.tytul} jedzie na ucztę....")
         else:
-            print("Lord pozostaje w pałacu....")
+            print(f"{self.tytul} pozostaje w pałacu....")
