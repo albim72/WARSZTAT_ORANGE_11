fe2324b27432050bed498628773c14704a10cdc7
albim72 <marcin.albiniak72@gmail.com>
2022-11-03 12:28:30 +0100

    Create main.py

---
+++ DZIEN_2/FITNESS_CLUB/main.py
@@ -0,0 +1,13 @@
+from osoba import Osoba
+
+os1 = Osoba("Feliks",43,98,176)
+os2 = Osoba()
+
+os1.print_osoba()
+os2.print_osoba()
+
+print(os1.czyjesttrenerem())
+print(os2.czyjesttrenerem())
+
+print("__________________________________________")
+
